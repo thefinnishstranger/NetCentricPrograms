8ad9992df1724c242d5287c59541fb1b882c1b76
Nikolas Gustavson <140442591+thefinnishstranger@users.noreply.github.com>
2024-09-29 19:07:56 -0500

    final changes

--- TCPServer.java
+++ TCPServer.java
@@ -9,15 +9,15 @@ public class TCPServer {
 
         ServerSocket serverSocket;
         try {
-            serverSocket = new ServerSocket(9999); // Creates a socket and binds it to port 9999
+            serverSocket = new ServerSocket(9999); //creates a socket and binds it to port 9999
             // serverSocket = new ServerSocket(0); // Creates a socket and binds it to next available port
             while (true) {
                 System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "...");
-                Socket connectionSocket = serverSocket.accept(); // Listens for connection and creates a connection socket for communication
+                Socket connectionSocket = serverSocket.accept(); //listens for connection and creates a connection socket for communication
                 System.out.println("Just connected server port # " + connectionSocket.getLocalSocketAddress() +
                         " to client port # " + connectionSocket.getRemoteSocketAddress());
 
-                DataInputStream in = new DataInputStream(connectionSocket.getInputStream()); // Get incoming data in bytes from connection socket
+                DataInputStream in = new DataInputStream(connectionSocket.getInputStream()); //get incoming data in bytes from connection socket
                 int fileSize = in.readInt();
                 byte[] fileBytes = new byte[fileSize];
                 in.readFully(fileBytes);
@@ -29,10 +29,10 @@ public class TCPServer {
                 System.out.println("Received file size in bits = " + (fileSize * 8));
                 System.out.println("Received file SHA256 hash: " + hashString);
 
-                DataOutputStream out = new DataOutputStream(connectionSocket.getOutputStream()); // Setup a stream for outgoing bytes of data
+                DataOutputStream out = new DataOutputStream(connectionSocket.getOutputStream()); //setup a stream for outgoing bytes of data
                 out.writeUTF(hashString);
 
-                connectionSocket.close(); // Close connection socket after this exchange
+                connectionSocket.close(); //close connection socket after this exchange
             }
         } catch (IOException e) {
             e.printStackTrace();
